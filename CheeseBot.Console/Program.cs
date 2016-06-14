970e22af614a7979ce5838f30ff076ab98730b2a
Sam Strong <sam.strong@nottingham.ac.uk>
2016-06-14 12:01:39 +0100

    Bunch of code.

---
+++ CheeseBot.Console/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using CheeseBot.Core;
+using Tweetinvi;
+using Tweetinvi.Core.Events.EventArguments;
+using Tweetinvi.Core.Interfaces.DTO;
+using Tweetinvi.Core.Parameters;
+using Stream = System.IO.Stream;
+
+namespace CheeseBot.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var twitterService = new TwitterService();
+
+            EventHandler<TweetReceivedEventArgs> tweetMatched = (sender, tweetArgs) =>
+            {
+                //System.Console.WriteLine(tweetArgs.Tweet.Text);
+
+                var tweetToReplyTo = tweetArgs.Tweet;
+
+                var assembly = Assembly.GetExecutingAssembly();
+                var stream = assembly.GetManifestResourceStream("CheeseBot.Console.Images.Applewood.jpg");
+                var imageBytes = ReadFully(stream);
+
+                var media = Upload.UploadImage(imageBytes);
+
+                var tweet = new PublishTweetParameters(
+                    $"@{tweetToReplyTo.CreatedBy.ScreenName} Applewood smoked cheddar goes great with everything!",
+                    new PublishTweetOptionalParameters
+                    {
+                        InReplyToTweetId = tweetToReplyTo.Id,
+                        Medias = new List<IMedia> { media }
+                    });
+
+                Tweet.PublishTweet(tweet);
+            };
+
+            try
+            {
+                twitterService.InitialiseMentionStream(tweetMatched);
+            }
+            catch(Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+            
+            System.Console.ReadLine();
+
+            twitterService.ShutDownStream();
+        }
+
+        public static byte[] ReadFully(Stream input)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ CheeseBot.Core/TwitterService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Tweetinvi;
+using Tweetinvi.Core.Events.EventArguments;
+using Tweetinvi.Core.Interfaces.Streaminvi;
+
+namespace CheeseBot.Core
+{
+    public class TwitterService
+    {
+        //private IFilteredStream _stream;
+        private IUserStream _userStream;
+
+        public TwitterService()
+        {
+            Auth.SetUserCredentials(
+                "8VbZaUz8aQsFnNWokBHz4O4xL",
+                "jtyiFMaBKwOmEjmOlSIVeESwzr2tK7tSiTy5lrU5iQ0OtMUoAP",
+                "736081535732846594-jIKv2rJrxNB2d6LLG5By6PJ2Fq4MapK",
+                "50d7YXzcga5MU3wRgD2fOXIfcwusBvEEAkcwZBLLZJoZs");
+        }
+
+        public void InitialiseMentionStream(EventHandler<TweetReceivedEventArgs> whenMentioned)
+        {
+            var _userStream = Stream.CreateUserStream();
+            _userStream.TweetCreatedByAnyoneButMe += whenMentioned;
+            _userStream.StartStream();
+        }
+
+        public void ShutDownStream()
+        {
+            _userStream.StopStream();
+        }
+    }
+}
